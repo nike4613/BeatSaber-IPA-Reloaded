8c2bff1349e3aa561ed5cc069869ce6d6686e159
Anairkoen Schno <aaron.cirr.com@gmail.com>
2018-07-30 20:28:35 -0500

    Added unity logger Documented new logging system

--- IllusionInjector/Bootstrapper.cs
+++ IllusionInjector/Bootstrapper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IllusionInjector.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,13 @@ namespace IllusionInjector
             {
                 Windows.GuiConsole.CreateConsole();
             }
+
+            Application.logMessageReceived += delegate (string condition, string stackTrace, LogType type)
+            {
+                var level = UnityLogInterceptor.LogTypeToLevel(type);
+                UnityLogInterceptor.Unitylogger.Log(level, $"{condition.Trim()}");
+                UnityLogInterceptor.Unitylogger.Log(level, $"{stackTrace.Trim()}");
+            };
         }
 
         void Start()
---
+++ IllusionInjector/Logging/UnityLogInterceptor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using LoggerBase = IllusionPlugin.Logging.Logger;
+
+namespace IllusionInjector.Logging
+{
+    public class UnityLogInterceptor
+    {
+        public static LoggerBase Unitylogger = new StandardLogger("UnityEngine");
+
+        public static LoggerBase.Level LogTypeToLevel(LogType type)
+        {
+            switch (type)
+            {
+                case LogType.Assert:
+                    return LoggerBase.Level.Debug;
+                case LogType.Error:
+                    return LoggerBase.Level.Error;
+                case LogType.Exception:
+                    return LoggerBase.Level.Critical;
+                case LogType.Log:
+                    return LoggerBase.Level.Info;
+                case LogType.Warning:
+                    return LoggerBase.Level.Warning;
+                default:
+                    return LoggerBase.Level.Info;
+            }
+        }
+    }
+}
