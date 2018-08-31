6755d08e53da607891371eed47416c5e4e3d199b
Anairkoen Schno <aaron.cirr.com@gmail.com>
2018-08-30 23:53:07 -0500

    Allowed IllusionPlugin and IllusionPlugin to see each others' internals

--- IllusionPlugin/Properties/AssemblyInfo.cs
+++ IllusionPlugin/Properties/AssemblyInfo.cs
@@ -21,6 +21,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e8cea89d-6c2f-4729-94b3-f355f7db19e5")]
+[assembly: InternalsVisibleTo("IllusionInjector")]
 
 // Version information for an assembly consists of the following four values:
 //
