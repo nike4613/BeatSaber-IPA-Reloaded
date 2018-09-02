220431f722d7c814aa924e43f15f20a2e5725284
Anairkoen Schno <aaron.cirr.com@gmail.com>
2018-09-01 20:45:26 -0500

    Moved all utilities to new location

--- IPA.Loader/Utilities/LoneFunctions.cs
+++ IPA.Loader/Utilities/LoneFunctions.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IllusionInjector.Utilities
+namespace IPA.Utilities
 {
     public static class LoneFunctions
     {
--- IPA.Loader/Utilities/SteamCheck.cs
+++ IPA.Loader/Utilities/SteamCheck.cs
@@ -1,11 +1,10 @@
-﻿using IllusionInjector.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IllusionInjector.Utilities
+namespace IPA.Utilities
 {
     public static class SteamCheck
     {
