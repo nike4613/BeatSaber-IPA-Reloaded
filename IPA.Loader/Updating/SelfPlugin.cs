37038e6f7844d916ad9454351b25ae13b2273bbd
Anairkoen Schno <aaron.cirr.com@gmail.com>
2018-09-23 23:35:14 -0500

    Increment Version

--- IPA.Loader/Updating/SelfPlugin.cs
+++ IPA.Loader/Updating/SelfPlugin.cs
@@ -11,7 +11,7 @@ namespace IPA.Updating
     internal class SelfPlugin : IBeatSaberPlugin
     {
         internal const string IPA_Name = "Beat Saber IPA";
-        internal const string IPA_Version = "3.10.3";
+        internal const string IPA_Version = "3.10.4";
 
         public static SelfPlugin Instance { get; set; } = new SelfPlugin();
 
