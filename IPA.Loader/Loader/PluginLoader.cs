1cdedfcc71d31f7156201e310c2bca6f4904ddcf
Anairkoen Schno <aaron.cirr.com@gmail.com>
2018-12-27 16:09:09 -0600

    Started rewrite of  loader

---
+++ IPA.Loader/Loader/PluginLoader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using Version = SemVer.Version;
+
+namespace IPA.Loader
+{
+    internal class PluginLoader
+    {
+        public class PluginMetadata
+        {
+            public Assembly Assembly;
+            public Type PluginType;
+            public string Name;
+            public Version Version;
+        }
+
+        public static void LoadMetadata()
+        {
+
+        }
+    }
+}
