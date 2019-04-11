ab6f80add028f30382ddc9b1e7b0d528b0935e62
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-04-11 18:50:21 -0500

    Bumped version Library loader now less strict about filenames

--- IPA.Loader/Config/SelfConfig.cs
+++ IPA.Loader/Config/SelfConfig.cs
@@ -33,7 +33,7 @@ namespace IPA.Config
         }
 
         internal const string IPAName = "Beat Saber IPA";
-        internal const string IPAVersion = "3.12.10"; 
+        internal const string IPAVersion = "3.12.11"; 
 		
         public bool Regenerate = true;
 
--- IPA/Program.cs
+++ IPA/Program.cs
@@ -23,7 +23,7 @@ namespace IPA
             Unknown
         }
 
-        public const string FileVersion = "3.12.10";
+        public const string FileVersion = "3.12.11";
 
         public static Version Version => Assembly.GetEntryAssembly().GetName().Version;
 
