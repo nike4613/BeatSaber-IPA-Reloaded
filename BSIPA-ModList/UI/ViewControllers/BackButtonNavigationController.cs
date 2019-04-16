7a1ee50389aa989e3ff28afbc720d67396d01500
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-04-16 00:33:19 -0500

    Redid menu to have a nav controller for better interaction

---
+++ BSIPA-ModList/UI/ViewControllers/BackButtonNavigationController.cs
@@ -0,0 +1,22 @@
+﻿using CustomUI.BeatSaber;
+using System;
+using UnityEngine.UI;
+using VRUI;
+
+namespace BSIPA_ModList.UI
+{
+    internal class BackButtonNavigationController : VRUINavigationController
+    {
+        public event Action didFinishEvent;
+
+        private Button _backButton;
+
+        protected override void DidActivate(bool firstActivation, ActivationType activationType)
+        {
+            if (firstActivation && activationType == ActivationType.AddedToHierarchy)
+            {
+                _backButton = BeatSaberUI.CreateBackButton(rectTransform, didFinishEvent.Invoke);
+            }
+        }
+    }
+}
