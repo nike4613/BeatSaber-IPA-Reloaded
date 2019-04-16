bf84bde6e87d71c7645ce8b702bff51c3d747037
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-04-15 23:15:15 -0500

    Added BSIPA-ModList project

---
+++ BSIPA-ModList/Plugin.cs
@@ -0,0 +1,65 @@
+﻿using IPA;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+using CustomUI.BeatSaber;
+using BSIPA_ModList.UI;
+using CustomUI.MenuButton;
+using UnityEngine.Events;
+
+namespace BSIPA_ModList
+{
+    internal static class Logger
+    {
+        internal static IPALogger log { get; set; }
+    }
+
+    public class Plugin : IBeatSaberPlugin
+    {
+        public void Init(IPALogger logger)
+        {
+            Logger.log = logger;
+            Logger.log.Debug("Init");
+        }
+
+        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+        {
+        }
+
+        public void OnApplicationQuit()
+        {
+        }
+
+        private ModListMenu menu;
+        private MenuButton button;
+
+        public void OnApplicationStart()
+        {
+            Logger.log.Debug("Creating Menu");
+        }
+
+        public void OnFixedUpdate()
+        {
+        }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        {
+            if (scene.name == "MenuCore" && button == null)
+            {
+                menu = BeatSaberUI.CreateCustomMenu<ModListMenu>("Installed Mods");
+                button = MenuButtonUI.AddButton("All Mods", "Shows all installed mods, along with controls for updating them.", () =>
+                {
+                    Logger.log.Debug("Presenting menu");
+                    menu.Present();
+                });
+            }
+        }
+
+        public void OnSceneUnloaded(Scene scene)
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+    }
+}
---
+++ BSIPA-ModList/UI/ModListMenu.cs
@@ -0,0 +1,26 @@
+﻿using CustomUI.BeatSaber;
+using IPA.Loader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSIPA_ModList.UI
+{
+    internal class ModListMenu : CustomMenu
+    {
+        private ModListController controller;
+
+#pragma warning disable CS0618
+        public ModListMenu()
+        {
+            Logger.log.Debug("Menu constructor");
+
+            controller = BeatSaberUI.CreateViewController<ModListController>();
+            controller.Init(PluginManager.AllPlugins, PluginLoader.ignoredPlugins, PluginManager.Plugins);
+            SetMainViewController(controller, true);
+        }
+#pragma warning restore
+    }
+}
