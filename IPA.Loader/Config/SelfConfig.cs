1df01414195caacecd5c400a5bbb465735a66d63
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-04-22 19:39:56 -0500

    Version bump

--- IPA.Loader/Config/SelfConfig.cs
+++ IPA.Loader/Config/SelfConfig.cs
@@ -33,7 +33,7 @@ namespace IPA.Config
         }
 
         internal const string IPAName = "Beat Saber IPA";
-        internal const string IPAVersion = "3.12.14"; 
+        internal const string IPAVersion = "3.12.15"; 
 		
         public bool Regenerate = true;
 
--- IPA/Program.cs
+++ IPA/Program.cs
@@ -23,7 +23,7 @@ namespace IPA
             Unknown
         }
 
-        public const string FileVersion = "3.12.14";
+        public const string FileVersion = "3.12.15";
 
         public static Version Version => Assembly.GetEntryAssembly().GetName().Version;
 
