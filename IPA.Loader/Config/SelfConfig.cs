6d3d8f89960882bab841eda9a8e99a1d981e2cdd
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-05-07 16:48:21 -0500

    Incremented BSIPA version StdoutInterceptor now initializes when the first message is sent through the log system

--- IPA.Loader/Config/SelfConfig.cs
+++ IPA.Loader/Config/SelfConfig.cs
@@ -33,7 +33,7 @@ namespace IPA.Config
         }
 
         internal const string IPAName = "Beat Saber IPA";
-        internal const string IPAVersion = "3.12.15"; 
+        internal const string IPAVersion = "3.12.16"; 
 		
         public bool Regenerate = true;
 
