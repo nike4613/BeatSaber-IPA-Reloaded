686f8794da7f4ad2e68dbd4ff106909ffe78dd1c
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-05-22 15:43:30 -0500

    Version bump

--- IPA.Loader/Config/SelfConfig.cs
+++ IPA.Loader/Config/SelfConfig.cs
@@ -37,7 +37,7 @@ namespace IPA.Config
         }
 
         internal const string IPAName = "Beat Saber IPA";
-        internal const string IPAVersion = "3.12.19"; 
+        internal const string IPAVersion = "3.12.20"; 
 		
         public bool Regenerate = true;
 
