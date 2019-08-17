1ba1e99953044cd2c9d1a088ae02977aafeae0e6
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-08-17 17:31:47 -0500

    Simplified AlmostVersion null check

--- IPA.Loader/Utilities/AlmostVersion.cs
+++ IPA.Loader/Utilities/AlmostVersion.cs
@@ -206,8 +206,8 @@ namespace IPA.Utilities
         /// <seealso cref="Equals(object)"/>
         public static bool operator==(AlmostVersion l, AlmostVersion r)
         {
-            if (ReferenceEquals(l, null) && ReferenceEquals(r, null)) return true;
-            if (ReferenceEquals(l, null) || ReferenceEquals(r, null)) return false;
+            if (l is null && r is null) return true;
+            if (l is null || r is null) return false;
             if (l.StorageMode != r.StorageMode) return false;
             if (l.StorageMode == StoredAs.SemVer)
                 return Utils.VersionCompareNoPrerelease(l.SemverValue, r.SemverValue) == 0;
