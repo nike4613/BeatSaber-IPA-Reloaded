4b9626d5f5c8cf436f676bf0cec1bda10e7bdbec
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-10-22 18:17:44 -0500

    Updated version numbers

--- IPA/Program.cs
+++ IPA/Program.cs
@@ -23,7 +23,7 @@ namespace IPA
             Unknown
         }
 
-        public const string FileVersion = "3.13.3";
+        public const string FileVersion = "3.99.99.0";
 
         public static Version Version => Assembly.GetEntryAssembly().GetName().Version;
 
