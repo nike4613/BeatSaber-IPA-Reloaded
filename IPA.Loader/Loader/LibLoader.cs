1d19c309224fd5f48f201f8d00a6e2d4a70d3d77
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-12-01 01:47:30 -0600

    Fixed load order Removed now-redundant debug logging

--- IPA.Loader/Loader/LibLoader.cs
+++ IPA.Loader/Loader/LibLoader.cs
@@ -53,8 +53,6 @@ namespace IPA.Loader
 
         internal static void Configure()
         {
-            Console.WriteLine("Configuring up library loading");
-
             SetupAssemblyFilenames(true);
             AppDomain.CurrentDomain.AssemblyResolve -= AssemblyLibLoader;
             AppDomain.CurrentDomain.AssemblyResolve += AssemblyLibLoader;
@@ -64,8 +62,6 @@ namespace IPA.Loader
         {
             if (FilenameLocations == null || force)
             {
-                Console.WriteLine("Calculating assembly filenames");
-
                 FilenameLocations = new Dictionary<string, string>();
 
                 foreach (var fn in TraverseTree(LibraryPath, s => s != NativeLibraryPath))
@@ -73,7 +69,6 @@ namespace IPA.Loader
                         Log(Logger.Level.Critical, $"Multiple instances of {fn.Name} exist in Libs! Ignoring {fn.FullName}");
                     else FilenameLocations.Add(fn.Name, fn.FullName);
 
-                foreach (var kvp in FilenameLocations) Console.WriteLine(kvp);
 
                 if (!SetDefaultDllDirectories(LoadLibraryFlags.LOAD_LIBRARY_SEARCH_USER_DIRS | LoadLibraryFlags.LOAD_LIBRARY_SEARCH_SYSTEM32
                                             | LoadLibraryFlags.LOAD_LIBRARY_SEARCH_DEFAULT_DIRS | LoadLibraryFlags.LOAD_LIBRARY_SEARCH_APPLICATION_DIR))
@@ -90,7 +85,7 @@ namespace IPA.Loader
                     if (retPtr == IntPtr.Zero)
                     {
                         var err = new Win32Exception();
-                        Log(Logger.Level.Warning, $"Could not add DLL directory");
+                        Log(Logger.Level.Warning, $"Could not add DLL directory {path}");
                         Log(Logger.Level.Warning, err);
                     }
                 }
@@ -123,8 +118,6 @@ namespace IPA.Loader
             Log(Logger.Level.Debug, $"Resolving library {asmName}");
 
             SetupAssemblyFilenames();
-
-            Console.WriteLine($"Looking for {asmName.Name} {asmName.Version} ({asmName.Name}.{asmName.Version}.dll)");
 
             var testFile = $"{asmName.Name}.{asmName.Version}.dll";
             Log(Logger.Level.Debug, $"Looking for file {testFile}");
