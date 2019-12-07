7d03a13419b2be937c186614372e96e21f0375f4
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-12-06 23:45:49 -0600

    Added some Net3 support stuff and implemented IConfigStore in GeneratedStore

---
+++ Net3-Proxy/Array.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OgArray = System.Array;
+
+namespace Net3_Proxy
+{
+    public static class Array
+    {
+        private static class EmptyArray<T>
+        {
+            public static readonly T[] Value = new T[0];
+        }
+
+        public static T[] Empty<T>() => EmptyArray<T>.Value;
+
+    }
+}
