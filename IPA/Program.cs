32086cfcfa1d43259f22dde9e615ca3ed640d20c
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-12-21 23:02:08 -0600

    Beta 2

--- IPA/Program.cs
+++ IPA/Program.cs
@@ -23,7 +23,7 @@ namespace IPA
             Unknown
         }
 
-        public const string FileVersion = "3.99.99.0";
+        public const string FileVersion = "3.99.99.1";
 
         public static Version Version => Assembly.GetEntryAssembly().GetName().Version;
 
