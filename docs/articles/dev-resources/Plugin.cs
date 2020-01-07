c50cd3aaabe9194347c40f9eb56cae6acf0621df
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-01-07 00:02:36 -0600

    Added a bit of demo code for start-dev

---
+++ docs/articles/dev-resources/Plugin.cs
@@ -0,0 +1,61 @@
+using System;
+using IPA;
+using IPA.Logging;
+using IPA.Config;
+using IPA.Config.Stores;
+
+namespace Demo
+{
+    /*
+    [Plugin(RuntimeOptions.DynamicInit)]
+    */
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    internal class Plugin
+    {
+        public static Logger log { get; private set; }
+
+        [Init]
+        public Plugin(Logger logger, Config conf)
+        {
+            log = logger;
+            PluginConfig.Instance = conf.Generated<PluginConfig>();
+            log.Debug("Config loaded");
+
+            // setup that does not require game code
+            // this is only called once ever, so do once-ever initialization
+        }
+
+        /*
+        [Init]
+        public Plugin(Logger logger)
+        {
+            log = logger;
+            log.Debug("Basic plugin running!");
+
+            // setup that does not require game code
+            // this is only called once ever, so do once-ever initialization
+        }
+        */
+
+        /*
+        [OnEnable]
+        public void OnEnable()
+        */
+        [OnStart]
+        public void OnStart()
+        {
+            // setup that requires game code
+        }
+
+        /*
+        [OnDisable]
+        public void OnDisable()
+        */
+        [OnExit]
+        public void OnExit()
+        {
+            // teardown
+            // this may be called mid-game if you are using RuntimeOptions.DynamicInit
+        }
+    }
+}
--- docs/articles/start-dev.md
+++ docs/articles/start-dev.md
@@ -3,5 +3,23 @@ uid: articles.start.dev
 title: Making your own mod
 ---
 
-# Overview
+# Making a mod
 
+## Overview
+
+What follows is a *very* barebones, and frankly not very useful plugin class, even as a starting point,
+but it should be enough to give a decent idea of how to do quick upgrades of existing mods for those who want to.
+
+[!code-cs[Plugin.cs](./dev-resources/Plugin.cs?range=1-3,6-8,12-16,29-37,39,44-45,46-49,54-57,59-)]
+
+There are basically 4 major concepts here:
+
+1. <xref:IPA.Logging.Logger>, the logging system.
+2. <xref:IPA.PluginAttribute>, which declares that this class is a plugin and how it should behave.
+3. <xref:IPA.InitAttribute>, which declares the constructor (and optionally other methods) as being
+   used for initialization.
+4. The lifecycle event attributes <xref:IPA.OnStartAttribute> and <xref:IPA.OnExitAttribute>.
+
+Read the docs at those links for a better idea of what they do.
+
+TODO: expand this to explain more, and expand on the base example
