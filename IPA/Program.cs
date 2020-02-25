ef16b0c90340c9c4e8512b01eddd8ed936695426
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-02-25 14:57:17 -0600

    Added names of new Beat Saber game assemblies

--- IPA/Program.cs
+++ IPA/Program.cs
@@ -23,7 +23,7 @@ namespace IPA
             Unknown
         }
 
-        public const string FileVersion = "3.99.99.5";
+        public const string FileVersion = "3.99.99.6";
 
         public static Version Version => Assembly.GetEntryAssembly().GetName().Version;
 
