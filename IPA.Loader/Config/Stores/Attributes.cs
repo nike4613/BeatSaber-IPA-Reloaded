c185b85e52fc2d07ee91bea30c9d5bd7404458ee
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-03-24 17:51:20 -0500

    Formatting

--- IPA.Loader/Config/Stores/Attributes.cs
+++ IPA.Loader/Config/Stores/Attributes.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IPA.Config.Stores.Attributes
 {
@@ -23,7 +20,7 @@ namespace IPA.Config.Stores.Attributes
 
     /// <summary>
     /// Indicates that a field or property in an object being wrapped by <see cref="GeneratedStore.Generated{T}(Config, bool)"/>
-    /// that would otherwise be nullable (i.e. a reference type or a <see cref="Nullable{T}"/> type) should never be null, and the 
+    /// that would otherwise be nullable (i.e. a reference type or a <see cref="Nullable{T}"/> type) should never be null, and the
     /// member will be ignored if the deserialized value is <see langword="null"/>.
     /// </summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
@@ -31,7 +28,7 @@ namespace IPA.Config.Stores.Attributes
 
     /// <summary>
     /// Indicates that a given field or property in an object being wrapped by <see cref="GeneratedStore.Generated{T}(Config, bool)"/>
-    /// should be serialized and deserialized using the provided converter instead of the default mechanism. 
+    /// should be serialized and deserialized using the provided converter instead of the default mechanism.
     /// </summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
     public sealed class UseConverterAttribute : Attribute
@@ -40,6 +37,7 @@ namespace IPA.Config.Stores.Attributes
         /// Gets the type of the converter to use.
         /// </summary>
         public Type ConverterType { get; }
+
         /// <summary>
         /// Gets the target type of the converter if it is avaliable at instantiation time, otherwise
         /// <see langword="null"/>.
@@ -60,7 +58,7 @@ namespace IPA.Config.Stores.Attributes
             ConverterType = converterType;
 
             var baseT = ConverterType.BaseType;
-            while (baseT != null && baseT != typeof(object) && 
+            while (baseT != null && baseT != typeof(object) &&
                 (!baseT.IsGenericType || baseT.GetGenericTypeDefinition() != typeof(ValueConverter<>)))
                 baseT = baseT.BaseType;
             if (baseT == typeof(object)) ConverterTargetType = null;
@@ -73,14 +71,14 @@ namespace IPA.Config.Stores.Attributes
     }
 
     /// <summary>
-    /// Specifies a name for the serialized field or property in an object being wrapped by 
+    /// Specifies a name for the serialized field or property in an object being wrapped by
     /// <see cref="GeneratedStore.Generated{T}(Config, bool)"/> that is different from the member name itself.
     /// </summary>
     /// <example>
     /// <para>
     /// When serializing the following object, we might get the JSON that follows.
     /// <code>
-    /// public class PluginConfig 
+    /// public class PluginConfig
     /// {
     ///     public virtual bool BooleanField { get; set; } = true;
     /// }
@@ -94,7 +92,7 @@ namespace IPA.Config.Stores.Attributes
     /// <para>
     /// However, if we were to add a <see cref="SerializedNameAttribute"/> to that field, we would get the following.
     /// <code>
-    /// public class PluginConfig 
+    /// public class PluginConfig
     /// {
     ///     [SerializedName("bool")]
     ///     public virtual bool BooleanField { get; set; } = true;
@@ -108,7 +106,7 @@ namespace IPA.Config.Stores.Attributes
     /// </para>
     /// </example>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
-    public sealed class SerializedNameAttribute : Attribute 
+    public sealed class SerializedNameAttribute : Attribute
     {
         /// <summary>
         /// Gets the name to replace the member name with.
@@ -124,6 +122,4 @@ namespace IPA.Config.Stores.Attributes
             Name = name;
         }
     }
-
-
 }
