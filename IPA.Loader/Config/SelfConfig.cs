0d67058090bdf3acbc45a4b2b8d20f7f9149580a
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-04-01 23:38:40 -0500

    Bumped version

--- IPA.Loader/Config/SelfConfig.cs
+++ IPA.Loader/Config/SelfConfig.cs
@@ -68,7 +68,7 @@ namespace IPA.Config
         }
 
         internal const string IPAName = "Beat Saber IPA";
-        internal const string IPAVersion = "4.0.3.0";
+        internal const string IPAVersion = "4.0.4.0";
 
         // uses Updates.AutoUpdate, Updates.AutoCheckUpdates, YeetMods, Debug.ShowCallSource, Debug.ShowDebug, 
         //      Debug.CondenseModLogs
