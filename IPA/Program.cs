190cc0150df43004a3da1870e86d90dbb6cef75e
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-09-02 19:03:20 -0500

    Bumped version

--- IPA/Program.cs
+++ IPA/Program.cs
@@ -23,7 +23,7 @@ namespace IPA
             Unknown
         }
 
-        public const string FileVersion = "4.1.1.0";
+        public const string FileVersion = "4.1.2.0";
 
         public static Version Version => Assembly.GetEntryAssembly().GetName().Version;
 
