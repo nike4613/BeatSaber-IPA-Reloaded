84b479c76b36fcc829b020388967577fd2c23a26
Kinsi <kinsi55@users.noreply.github.com>
2022-07-21 07:38:40 +0200

    space

--- IPA.Loader/Logging/StandardLogger.cs
+++ IPA.Loader/Logging/StandardLogger.cs
@@ -122,7 +122,8 @@ namespace IPA.Logging
             PrintFilter = SelfConfig.Debug_.ShowDebug_ ? LogLevel.All : LogLevel.InfoUp;
             showTrace = SelfConfig.Debug_.ShowTrace_;
             syncLogging = SelfConfig.Debug_.SyncLogging_;
-            if(SelfConfig.CommandLineValues.WriteLogs && !addedFilePrinter) {
+            if (SelfConfig.CommandLineValues.WriteLogs && !addedFilePrinter)
+            {
                 addedFilePrinter = true;
                 AddDefaultPrinter(new GlobalLogFilePrinter());
             }
